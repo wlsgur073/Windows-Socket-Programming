6fbe99705c73ba43c9488623e97d2af117dafe3f
wlsgur073 <wlsgur073@gmail.com>
2025-01-05 15:01:39 +0900

    Test TCP server and client, debugging with wireshark

---
+++ HelloTcpipClient/HelloTcpipClient.cpp
@@ -0,0 +1,63 @@
+#include "stdafx.h"
+#include <winsock2.h>
+#pragma comment(lib, "ws2_32")
+
+// Keep in mind. to terminating a TCP connection lies with the client.
+// The side that initiates the disconnection will enter the TIME_WAIT state.
+// Therefore, the server should not be in the TIME_WAIT state.
+int _tmain(int argc, _TCHAR* argv[])
+{
+	//※윈속 초기화
+	WSADATA wsa = { 0 };
+	if (::WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+	{
+		puts("ERROR: 윈속을 초기화 할 수 없습니다.");
+		return 0;
+	}
+
+	//1. 접속대기 소켓 생성
+	SOCKET hSocket = ::socket(AF_INET, SOCK_STREAM, 0); // client also has a socket
+	if (hSocket == INVALID_SOCKET)
+	{
+		puts("ERROR: 소켓을 생성할 수 없습니다.");
+		return 0;
+	}
+
+	//2. 포트 바인딩 및 연결
+	// where set the client socket's address ? -> OS will assign the client address
+	SOCKADDR_IN	svraddr = { 0 }; // server socket address
+	svraddr.sin_family = AF_INET;
+	svraddr.sin_port = htons(25000);
+	svraddr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+	if (::connect(hSocket, // client socket must to know server socket ip and port
+		(SOCKADDR*)&svraddr, sizeof(svraddr)) == SOCKET_ERROR)
+	{
+		puts("ERROR: 서버에 연결할 수 없습니다.");
+		return 0;
+	}
+
+	//3. 채팅 메시지 송/수신
+	char szBuffer[128] = { 0 };
+	while (1)
+	{
+		//사용자로부터 문자열을 입력 받는다.
+		gets_s(szBuffer);
+		if (strcmp(szBuffer, "EXIT") == 0) break;
+
+		//사용자가 입력한 문자열을 서버에 전송한다.
+		::send(hSocket, szBuffer
+			, strlen(szBuffer) + 1 // why +1 ? -> include `null` character
+			, 0);
+		//서버로부터 방금 보낸 문자열에 대한 에코 메시지를 수신한다.
+		memset(szBuffer, 0, sizeof(szBuffer));
+		::recv(hSocket, szBuffer, sizeof(szBuffer), 0);
+		printf("From server: %s\n", szBuffer);
+	}
+
+	//4. 소켓을 닫고 종료.
+	//::shutdown(hSocket, SD_BOTH); // means stop I/O
+	::closesocket(hSocket);
+	//※윈속 해제
+	::WSACleanup();
+	return 0;
+}
---
+++ HelloTcpipServer/HelloTcpip.cpp
@@ -0,0 +1,90 @@
+#include "stdafx.h"
+#include <winsock2.h>
+#pragma comment(lib, "ws2_32")
+
+// https://learn.microsoft.com/en-us/windows/win32/api/winsock2/nf-winsock2-socket
+int _tmain(int argc, _TCHAR* argv[])
+{
+	// Initialize WinSock, WSA(Windows Sockets API)
+	WSADATA wsa = { 0 };
+	if (::WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+	{
+        puts("ERROR: Unable to initialize WinSock.");
+		return 0;
+	}
+
+	// 1. Create a socket for listening
+	SOCKET hSocket = ::socket(
+		AF_INET // L3: Protocol Family - IPv4
+		, SOCK_STREAM // L4: Protocol - TCP
+		, 0 // L4 Protocol: TCP
+	);
+	if (hSocket == INVALID_SOCKET)
+	{
+        puts("ERROR: Unable to create a listening socket.");
+		return 0;
+	}
+
+	// 2. port binding
+	SOCKADDR_IN	svraddr = { 0 };
+	svraddr.sin_family = AF_INET;
+    // IP addresses and ports are all numbers. The way computers handle numbers is called Endian.
+	// CPUs like Intel use Little Endian. Networks use Big Endian.
+	// socket APIs use Big Endian. So, we need to convert Little Endian to Big Endian.
+	svraddr.sin_port = htons(25000); // standard port for 32bit
+	svraddr.sin_addr.S_un.S_addr = htonl(INADDR_ANY); // if server has multiple IP addresses, client can connect to any of them.
+	if (::bind(hSocket, (SOCKADDR*)&svraddr, sizeof(svraddr)) == SOCKET_ERROR)
+	{
+        puts("ERROR: Unable to bind IP address and port to the socket.");
+		return 0;
+	}
+
+    // 3. Switch to listening state
+	if (::listen(hSocket
+		, SOMAXCONN // maximum number of clients that can be queued in OS
+	) == SOCKET_ERROR)
+	{
+        puts("ERROR: Unable to switch to listening state.");
+		return 0;
+	}
+
+    // 4. Client connection handling and response
+	SOCKADDR_IN clientaddr = { 0 }; // address of the client
+	int nAddrLen = sizeof(clientaddr);
+	SOCKET hClient = 0; // client socket
+	char szBuffer[128] = { 0 };
+	int nReceive = 0; // number of bytes received
+
+    // 4.1 Accept client connection and create a new socket
+	while ((hClient = ::accept(
+		hSocket
+		, (SOCKADDR*)&clientaddr
+		, &nAddrLen)) != INVALID_SOCKET)
+	{
+        puts("A new client has connected."); fflush(stdout);
+        // 4.2 Receive a string from the client.
+		while ((nReceive = ::recv(hClient, szBuffer, sizeof(szBuffer), 0)) > 0) // if recv returns 0, the client has disconnected
+		{
+            // 4.3 Echo the received string back to the client.
+			::send(hClient
+				, szBuffer
+				, sizeof(szBuffer) // even if nReceive is smaller than sizeof(szBuffer), we send the whole buffer
+				, 0);
+			puts(szBuffer); fflush(stdout);
+			memset(szBuffer, 0, sizeof(szBuffer));
+		}
+
+        // 4.3 Client has disconnected.
+		//::shutdown(hSocket, SD_BOTH); // do not shutdown the listening socket
+		::shutdown(hClient, SD_BOTH);
+		::closesocket(hClient);
+        puts("Client connection has been terminated."); fflush(stdout);
+	}
+
+	// 5. close the listening socket
+	::closesocket(hSocket);
+
+    // Cleanup WinSock
+	::WSACleanup();
+	return 0;
+}
